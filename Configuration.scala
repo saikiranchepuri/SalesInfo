cc9295ae01a27c7c2d7a15d8cf791cd3fed9c98f
Saikiran Chepuri <saikiranchepuri35@gmail.com>
2020-04-08 19:02:25 +0530

    Update Configuration.scala

--- Configuration.scala
+++ Configuration.scala
@@ -3,6 +3,5 @@ package com.epam.sales
 import org.apache.spark.sql.SparkSession
 
 trait Configuration {
-  val spark = SparkSession.builder().master("yarn").appName("SalesInfo")
-    .config("spark.sql.warehouse.dir","/apps/hive/warehouse").config("hive.metastore.uris","thrift://rm01.itversity.com:9083").enableHiveSupport().getOrCreate()
+  val spark = SparkSession.builder().master("yarn").appName("SalesInfo").getOrCreate()
 }
